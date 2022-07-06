7549568faa5ef7965952544e8af5a774a8445b00
sangwoong03 <sangwoong03@gmail.com>
2022-07-06 11:27:05 +0900

    Update: modified codes based on reviews

--- reviews/models.py
+++ reviews/models.py
@@ -5,11 +5,11 @@ from users.models import User
 from rooms.models import Room
 
 class Review(TimeStampModels):
-    content    = models.CharField(max_length=500)
-    review_img = models.CharField(max_length=1000)
-    ratings    = models.DecimalField(max_digits=2, decimal_places=2, default=3.0)
-    user       = models.ForeignKey(User, on_delete=models.CASCADE, )
-    room       = models.ForeignKey(Room, on_delete=models.CASCADE,)
+    content   = models.CharField(max_length=500)
+    image_url = models.CharField(max_length=1000)
+    ratings   = models.DecimalField(max_digits=2, decimal_places=2, default=0)
+    user      = models.ForeignKey(User, on_delete=models.CASCADE)
+    room      = models.ForeignKey(Room, on_delete=models.CASCADE)
     
     class Meta:
         db_table = "reviews"
