7efcc1deb40941ea2858b57d3711892a01a13a21
sangwoong03 <sangwoong03@gmail.com>
2022-07-12 12:42:02 +0900

    Add: completed review POST

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from core.utils   import token_decorator
+from django.http  import JsonResponse
+from django.views import View
+
+from reviews.models import Review
+
+class ReviewView(View):
+    @token_decorator
+    def post(self, request):
+        try:
+            data      = json.loads(request.body)
+            content   = data["content"]
+            ratings   = data["ratings"]
+            image_url = data["image_url"]
+            room_id   = data["room_id"]
+            user      = request.user
+
+            Review.objects.create(
+                content   = content,
+                ratings   = ratings,
+                image_url = image_url,
+                user_id   = user.id,
+                room_id   = room_id
+            )
+            return JsonResponse({"message": "SUCCESS"}, status=200)
+
+        except KeyError:
+           return  JsonResponse({"message": "KEY_ERROR"}, status=400)
--- tamnabnb/urls.py
+++ tamnabnb/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("users", include("users.urls")),
-    path("rooms", include("rooms.urls"))
+    path("rooms", include("rooms.urls")),
+    path("reviews", include("reviews.urls"))
 ]
